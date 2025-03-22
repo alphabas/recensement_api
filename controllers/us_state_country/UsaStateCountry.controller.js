02ec05dfc6ea5b14de346a5fc589cdd2ea89fea0
alphabas <alphabas22@gmail.com>
2025-03-22 08:20:31 +0300

    wip

--- controllers/us_state_country/UsaStateCountry.controller.js
+++ controllers/us_state_country/UsaStateCountry.controller.js
@@ -1,8 +1,10 @@
 
 const shapefile = require('shapefile');
 const path = require('path');
+const axios = require('axios');
 const RESPONSE_CODES = require('../../constants/RESPONSE_CODES');
 const RESPONSE_STATUS = require('../../constants/RESPONSE_STATUS');
+const userName = "alphabas"; 
 
 const readShpFile = async (fileName) => {
     const pathToShp = path.join('public/us_state_data/', fileName);
@@ -25,8 +27,71 @@ const readShpFile = async (fileName) => {
 };
 
 
+// GET DENSITY WITH LAT AND LONG
+async function getLoadingDensity(latitude, longitude) {
+    try {
+        const response = await axios.get(`http://api.geonames.org/findNearbyPlaceNameJSON?lat=${latitude}&lng=${longitude}&username=${userName}`);
+        
+        if (response.data && response.data.geonames && response.data.geonames.length > 0) {
+            const place = response.data.geonames[0];
+            return place.population; 
+        } else {
+            console.log('No pop');
+            return null;
+        }
+    } catch (error) {
+        console.error('Error fetching population data:', error);
+        return null;
+    }
+}
+
+
+async function getDensity(latitude, longitude, areaDensity) {
+    const population = await getLoadingDensity(latitude, longitude);
+
+    if (population !== null) {
+        const density = calculateDensity(population, areaDensity);
+        return density 
+    } else {
+        console.log('Could not retrieve population data.');
+    }
+}
+
+
+const getZoomLevel = (density) => {
+    let zoomLevel;
+  
+    switch (true) {
+      case (density <= 60):
+        zoomLevel = 6;  // Low density
+        break;
+      case (density <= 150):
+        zoomLevel = 7;  // Medium density
+        break;
+      case (density <= 300):
+        zoomLevel = 10; // High density
+        break;
+      default:
+        zoomLevel = 12; // My def densityy
+    }
+  
+    return zoomLevel;
+  }
+
+
+
+  const runingIsBusinessArea = (block) => {
+    const businessLandUses = ["Commercial", "Mixed-use", "Industrial"];
+    const businessIndicators = block.dens > 60 ;
+  
+    return businessLandUses.includes(block.landUse) || businessIndicators;
+  };
+
+
+
+
 const getCurrentDataCountry = async (req, res) => {
-    const { rows, first, sortField, sortOrder, search, latitude, longitude } = req.query;
+    const { rows, first, sortField, sortOrder, search, latitude, longitude, zoom, business_type, areaDensity } = req.query;
     
 
     try {
@@ -40,10 +105,11 @@ const getCurrentDataCountry = async (req, res) => {
         //GROUP AND  DELETE THE DOUBLONS
         const currentElement = paginatedData.reduce((acc, feature) => {
             const coords = feature.geometry.coordinates;
-
+            const areaData = 9.8e6;
             coords.forEach(polygon => {
-                polygon.forEach(coord => {
-                    const latLong = { lat: coord[0], long: coord[1] };
+                polygon.forEach(async coord => {
+                    const densElement = await getDensity(coord[0], coord[1],areaData); 
+                    const latLong = { lat: coord[0], long: coord[1], dens : densElement };
                     // VER DOUBLON
                     const exists = acc.some(item => item.lat === latLong.lat && item.long === latLong.long);
                     if (!exists) {
@@ -54,9 +120,30 @@ const getCurrentDataCountry = async (req, res) => {
             return acc;
         }, []);
 
+        // Filter who the population density and ajust zoom level > 100
+        const arr = []  
+        const filteredData = currentElement.forEach(item => {
+            if(item.dens > 50){
+                const zoom = getZoomLevel(item.dens);
+                arr.push({
+                    lat : item.lat,
+                    long : item.long,
+                    density: item.dens,
+                    zoom : zoom,
+                    landUse : item?.landUse 
+                })
+                
+            }
+            
+          });
+        
+
+
+        // Filter the census blocks Remove Census Blocks that are unlikely to contain businesses and Prioritize urban and commercial areas.
+        const filteredBlocks = filteredData.filter(block => runingIsBusinessArea(block));
 
         // Filter 
-        const filteredElements = currentElement.filter(item => {
+        const filteredElements = filteredBlocks.filter(item => {
             const latCondition = latitude 
               ? String(item.lat).includes(latitude) 
               : true; 
